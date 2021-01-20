8af600f5134ec00c4b87bd2b8718f285ea2de640
Patrick Cool <4230215+Pslice@users.noreply.github.com>
2021-01-20 13:09:16 -0800

    Updated with source server file

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 //js
+//*create the repository in github and copy the coded path.git link
+//git clone [path.git]
+//*after making changes*
+//git add .
+//git commit -m "commit comments"
+//git push
